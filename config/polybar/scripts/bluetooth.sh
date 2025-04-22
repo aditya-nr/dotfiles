1bb4c03fbed33ad6df1ba0ca47e169806efda90b
Aditya NR <adityanarayanrajan@gmail.com>
2025-04-22 12:35:50 +0530

    wallpapers

--- config/polybar/scripts/bluetooth.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-status=$(bluetoothctl show | grep "Powered" | awk '{print $2}')
-[ "$status" == "yes" ] && echo "On" || echo "Off"
-
--- config/polybar/scripts/camera.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-brightness=$(brightnessctl | grep -oP '\(\K[0-9]+(?=%\))')
-echo " ${brightness}%"
-
